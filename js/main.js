9e3554a3a483d761a6d0193dddeb138a69e103ed
Luka Milovanovic <lukamilovanovic98@gmail.com>
2019-01-30 12:23:40 +0100

    added header

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".toggle").click(function(){
+        $(".toggle").toggleClass("active");
+        $(".sidebar").toggleClass("active");
+    });
+
+});
+
+
+
+
+
+
+
